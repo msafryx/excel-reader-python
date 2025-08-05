6098915be1ca8f5f9a8a3e048355b5ed9ba0072d
msafryx <mzafry2002@gmail.com>
2025-08-05 17:26:30 +0530

    initial commit

---
+++ excel_reader.py
@@ -0,0 +1,8 @@
+import openpyxl as xl
+
+wb = xl.load_workbook('data/transactions.xlsx')
+sheet =  wb['Sheet1']
+# cell = sheet['a1']
+cell = sheet.cell(1,1)
+# print(cell.value)
+# print(sheet.max_row)
